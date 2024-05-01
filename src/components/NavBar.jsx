57e5cdcf64de05f9c296109ffb2c659755c41e12
Shivam <shivamdave2903@gmail.com>
2024-05-01 20:08:54 +0530

    done

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const NavBar = () => {
   return (
-    <nav className="fixed top-0 w-full bg-black bg-opacity-50  py-8 px-10 flex justify-between items-center  z-10">
-      <h1 className='text-4xl font-greek text-white'>Kaabil.me</h1>
+    <nav className="fixed top-0 w-full bg-black bg-opacity-0 mt-10 py-8 px-10 flex justify-center items-center  z-10">
+      <h1 className='text-4xl font-greek text-white'>Kaabil.<span className="bg-gradient-to-r from-orange-500 to-red-800 text-transparent bg-clip-text">me</span></h1>
       <div className="flex space-x-6 text-white">
         {/* <a href="#about" className="hover:text-gray-300">About</a> */}
         {/* <a href="#contact" className="hover:text-gray-300">Contact</a> */}
