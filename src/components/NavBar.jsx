c3a943f3fb5da9794e5c92e5ce62edb55c72b7b0
Prince  Doshi <Prince@Princes-MacBook-Air.local>
2024-05-02 18:19:56 +0530

    responsive

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NavBar = () => {
   return (
-    <nav className="fixed top-0 w-full bg-black bg-opacity-0 py-4 px-4 flex justify-center items-center z-10 sm:px-10 sm:py-8">
+    <nav className="relative mt-10 top-0 w-full bg-black bg-opacity-0 py-4 px-4 flex justify-center items-center z-10 sm:px-10 sm:py-8">
       <div className="flex items-center">
         <h1 className="text-2xl font-greek text-white sm:text-4xl">
           Kaabil.<span className="bg-gradient-to-r from-orange-500 to-red-800 text-transparent bg-clip-text">me</span>
